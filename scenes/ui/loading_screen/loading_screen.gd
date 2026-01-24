bcf62c94c8e6f03bb2f9d0fcc1223b54a35add09
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-01-24 15:24:17 +0100

    idk

--- scenes/ui/loading_screen/loading_screen.gd
+++ scenes/ui/loading_screen/loading_screen.gd
@@ -2,23 +2,68 @@ extends CanvasLayer
 
 signal loading_screen_has_full_coverage
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export_category("Video Files")
+@export var file_fill: String = "res://0001-0075.ogv"
+@export var file_empty: String = "res://0075-0150.ogv"
+
+# Reference both players
+@onready var player_fill: VideoStreamPlayer = $VideoLayer_Fill
+@onready var player_empty: VideoStreamPlayer = $VideoLayer_Empty
 @onready var progress_bar: ProgressBar = $Panel/ProgressBar
 
+var _is_loading_finished: bool = false
+var _fill_finished: bool = false
+
+func _ready() -> void:
+	# Setup Player Empty (The second one)
+	# We load it early, but keep it hidden or paused
+	player_empty.stream = load(file_empty)
+	player_empty.hide() # Hide it so it doesn't interfere yet
+	
+	# Setup Player Fill (The first one)
+	player_fill.stream = load(file_fill)
+	player_fill.finished.connect(_on_fill_finished)
+	player_fill.play()
 
 func _update_progress_bar(new_value: float) -> void:
 	progress_bar.set_value_no_signal(new_value * 100)
 
-
 func _start_outro_animation() -> void:
-	if animation_player.is_playing():
-		animation_player.stop()
+	_is_loading_finished = true
+	_check_and_start_outro()
+
+# --- Logic ---
+
+func _on_fill_finished() -> void:
+	_fill_finished = true
 	
-	animation_player.play("loading_end")
+	# FREEZE the top player. It now acts as a static "screenshot" 
+	# covering the transition logic.
+	player_fill.paused = true
 	
-	if animation_player.has_animation("loading_end"):
-		await animation_player.animation_finished
-	else:
-		push_warning("Animation 'loading_end' not found!")
+	loading_screen_has_full_coverage.emit()
+	_check_and_start_outro()
+
+func _check_and_start_outro() -> void:
+	if _fill_finished and _is_loading_finished:
+		_transition_to_empty()
+
+func _transition_to_empty() -> void:
+	# 1. Prepare the bottom player (Empty)
+	player_empty.show()
+	player_empty.play()
 	
-	self.queue_free()
+	# 2. WAIT A FRAME (Crucial!)
+	# We need to give the video decoder 1-2 frames to actually put 
+	# pixels on the texture. If we hide the top layer immediately, 
+	# we might still see a black flash from the bottom layer starting up.
+	await get_tree().process_frame
+	await get_tree().process_frame
+	
+	# 3. Now that the bottom video is definitely running, delete the top one.
+	# The user won't notice the switch because the images should be identical.
+	player_fill.queue_free()
+	
+	# 4. Handle end of second video
+	await player_empty.finished
+	queue_free()
