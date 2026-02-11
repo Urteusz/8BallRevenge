0a61d731a1e7066cade955081aeef5914bab79b1
Urteusz <161340944+Urteusz@users.noreply.github.com>
2026-02-11 22:27:42 +0100

    Revert "Merge branch 'cube-karta-co-lata' into urteusz"
    This reverts commit ca867c3fa0ddf9a788efe9b163723044a8278a5d, reversing
    changes made to d375582849a124373bf38851e46fd921478f1cd8.

--- scenes/ui/card/card.gd
+++ scenes/ui/card/card.gd
@@ -1,24 +1,24 @@
-extends Node3D
+extends SubViewportContainer
 
+# pewnie zamiast uzywac export, powinien dostawac kule z listy kul
 @export var card: Node3D
 @export var camera: Camera3D
-@export var second_camera: Camera3D
 @export var ball: Node3D
-@export var target: Node3D
-@export var card_rotation_sensitivity: float = 0.2
-@export var lerp_speed: float = 10.0
+# Zmienia jak daleko/blisko jest Z pozycja kursora,
+#	< 1.0 -> karta bardziej sie obraca
+#	> 1.0 -> karta obraca sie mniej
+@export var card_rotation_sensitivity: float = 1.0
 
 var ball_previous_position: Vector3
 
 func _ready() -> void:
 	ball_previous_position = ball.position
 
+
 func _process(delta: float) -> void:
-	var destination = second_camera.project_position(Vector2.ZERO, 1.0) + Vector3(0.5, -0.5, 0.0)
-	var look_target = destination + (destination - target.global_position)
-	
-	var target_transform = Transform3D(Basis(), destination)
-	target_transform = target_transform.looking_at(look_target, Vector3.UP)
-	target_transform.basis = target_transform.basis.scaled(Vector3.ONE * 0.3)
-	
-	card.global_transform = card.global_transform.interpolate_with(target_transform, delta * lerp_speed)
+	var mouse_position: Vector2 = get_global_mouse_position()
+	var window_size = DisplayServer.window_get_size()
+	var mouse_position_x: float = remap(mouse_position.x, 0, window_size.x, 1.0, -1.0);
+	var mouse_position_y: float = remap(mouse_position.y, 0, window_size.y, -1.0, 1.0);
+	var target = Vector3(mouse_position_x, mouse_position_y, -camera.position.z * card_rotation_sensitivity)
+	card.look_at(target, Vector3.UP, false)
